cf4d95580e6dc9dd57bfcebd29a30e9bc1b9995b
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-02-22 23:02:18 +0900

    옹알이 문제 풀이

--- 2302/MFGang/programers_01.py
+++ 2302/MFGang/programers_01.py
@@ -1,23 +1,17 @@
 def solution(babbling):
-    babbling_list = ['aya', 'ye', 'woo', 'ma']
-    temp_babbling = []
+    check_list = ["aya", "ye", "woo", "ma"]
     answer = 0
-    for i in range(babbling):
-        for k in range(babbling_list):
-            A = babbling_list[k]
-            B = babbling[i]
-            if A in B:
-                temp_babbling.append(A)
-                if babbling_list[k+1] in B:
-                    temp_babbling.append(A)
-                elif babbling_list[k+2] in B:
-                    temp_babbling.append(A)
-                elif babbling_list[k+3] in B:
-                    temp_babbling.append(A)
-        if temp_babbling[i] == B:
-            answer += 1 
-            
-babbling = list(map(str, ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]))
-
-if __name__ == '__main__':
-    print(solution(babbling))
+    for i in range(len(babbling)):
+        BB = babbling[i]        
+        for j in range(4):
+            CL = check_list[j]
+            if CL in BB:
+                BB = BB.replace(CL,' ')
+                babbling[i] = BB
+            elif BB > '':
+                pass
+        if babbling[i].strip() == '':
+            answer += 1
+        else:
+            continue
+    return answer
